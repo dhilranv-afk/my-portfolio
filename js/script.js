18ce448807ad3fd0b748aa0f11ff849162a3f48c
dhilranv-afk <dhilranv@gmail.com>
2026-03-12 19:39:12 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -73,3 +73,31 @@ new Typed("#typing", {
   loop:true
 
 });
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 50){
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+});
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+
+  });
+
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
