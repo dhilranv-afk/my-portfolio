735368dbee473cb4549509a48b1f5ded2f969f36
dhilranv-afk <dhilranv@gmail.com>
2026-04-19 18:37:25 -0400

    Add scroll event listener for progress bar

--- js/script.js
+++ js/script.js
@@ -77,3 +77,12 @@ function typeEffect() {
 
 // Start typing after page loads
 window.addEventListener("load", typeEffect);
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  const progress = (scrollTop / scrollHeight) * 100;
+
+  document.getElementById("progress-bar").style.width = progress + "%";
+});
